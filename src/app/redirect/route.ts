9f15f7c420e197768b5e56a7ba6ca602799b5b5e
r00t-creditninja <vbazyliak@creditninja.com>
2025-08-27 05:49:43 -0700

    added base64 decode

--- src/app/redirect/route.ts
+++ src/app/redirect/route.ts
@@ -9,8 +9,10 @@ export async function GET(req: NextRequest) {
     return NextResponse.json({ error: "Missing token" }, { status: 400 });
   }
 
+  const decoded = Buffer.from(token, "base64").toString("utf-8");
+
   // Decrypt the token to get the original URL
-  const decrypted = await decrypt(token);
+  const decrypted = await decrypt(decoded);
   return NextResponse.redirect(decrypted);
 }
 export async function decrypt(jwe: string) {
